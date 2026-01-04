17684d5b3f46a82d859dcbb43d72dcb408fc252f
pavanreddy565 <pavanreddy5658@gmail.com>
2026-01-04 21:17:19 +0530

    port changes

--- client/src/config/api.js
+++ client/src/config/api.js
@@ -4,6 +4,6 @@ export const LOCAL_API =
 // Peer API will be discovered dynamically
 export let PEER_API = null;
 
-export function setPeerApi(ip, port = 5000) {
+export function setPeerApi(ip, port = 5050) {
   PEER_API = `http://${ip}:${port}`;
 }
